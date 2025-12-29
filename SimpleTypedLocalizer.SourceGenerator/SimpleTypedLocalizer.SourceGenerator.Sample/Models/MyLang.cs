d6df4afab992a76f1cec156e3a205fc8df0ea28c
MikiraSora <mikirasora0409@126.com>
2025-12-29 21:32:23 +0800

    fix doc

--- SimpleTypedLocalizer.SourceGenerator/SimpleTypedLocalizer.SourceGenerator.Sample/Models/MyLang.cs
+++ SimpleTypedLocalizer.SourceGenerator/SimpleTypedLocalizer.SourceGenerator.Sample/Models/MyLang.cs
@@ -2,7 +2,7 @@
 
 namespace SimpleTypedLocalizer.SourceGenerator.Sample.Models;
 
-//[ImportLocalizedTextFiles("Assets/Languages/lang.json", ImportType.DumpAsStaticDictionaryInCompileTime)]
+[ImportLocalizedTextFiles("Assets/Languages/lang.json", ImportType.DumpAsStaticDictionaryInCompileTime)]
 public partial class MyLang
 {
     
